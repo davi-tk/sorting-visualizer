6166d02a0655e3bbbd0709b05418920607a587b3
Davi Dantas <davi.soares02@gmail.com>
2023-05-25 12:47:24 -0300

    feat : render random array and as bars

---
+++ src/components/Bar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :style="barStyle">
+        {{ size }}
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    size : number
+}>()
+
+const barStyle = {
+    height: `${props.size + 2}rem`,
+    width: '2rem',
+    backgroundColor : 'aliceblue',
+    color: 'red',
+}
+
+</script>
+
+<style scoped>
+
+</style>
